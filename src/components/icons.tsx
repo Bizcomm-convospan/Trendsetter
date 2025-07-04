c87b5eb8cf51ca1813261b1580c597dc65fed88b
Bizcomm solutions <bizcomm.solutions@gmail.com>
2025-07-04 05:59:00 +0000

    ok add element of AI in this

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -14,6 +14,10 @@ export function TrendsetterProLogo(props: SVGProps<SVGSVGElement>) {
     >
       <path d="M3 17L9 11l4 4 8-8" />
       <polyline points="15 7 21 7 21 13" />
+
+      {/* AI Sparkles */}
+      <path d="M12.5 2.5 14 5l-1.5 1.5L11 5z" />
+      <path d="m5 11 1.5 1.5L5 14l-1.5-1.5z" />
     </svg>
   );
 }
